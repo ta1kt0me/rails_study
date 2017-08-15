91a6f64ce3cb639f75e795a3b88d8f9f7088dc69
ta1kt0me <p.wadachi@gmail.com>
2017-08-15 12:11:31 +0900

    Eanble start up rails server

---
+++ db/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE rails_development;
+CREATE DATABASE rails_test;
+GRANT ALL PRIVILEGES ON DATABASE rails_development TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE rails_test TO postgres;
