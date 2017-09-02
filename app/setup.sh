edfbc33ee879c81e749ed52fc1992c540f078d65
ta1kt0me <p.wadachi@gmail.com>
2017-09-02 23:56:18 +0900

    rails 1.2.0

--- app/setup.sh
+++ app/setup.sh
@@ -9,7 +9,7 @@ if [ ! -d "/apps/todo" ]; then
   mkdir -p db/migrate
   cp /tmp/database.yml config/database.yml
   cp /tmp/001_create_tasks.rb db/migrate/001_create_tasks.rb
-
+  sed  -i -e 's/require_gem\(.*\)/gem\1/' config/boot.rb
   rake migrate
   script/generate scaffold Task
   echo 'created new project'
