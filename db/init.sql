35c50285198b1024733123d98ec030318acbbf6a
ta1kt0me <p.wadachi@gmail.com>
2018-02-25 22:48:32 +0900

    Remove an useless container

--- db/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE rails_development;
-CREATE DATABASE rails_test;
-GRANT ALL PRIVILEGES ON DATABASE rails_development TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE rails_test TO postgres;
