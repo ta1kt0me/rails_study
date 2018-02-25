a0eea056fb716a798272d74a2909bfc49aecbf7a
ta1kt0me <p.wadachi@gmail.com>
2018-02-25 21:51:05 +0900

    Create sample app in rails 3.0

--- app/setup.sh
+++ app/setup.sh
@@ -4,12 +4,13 @@ set -e
 
 if [ ! -d "/apps/todo" ]; then
   cd /apps
-  rails todo
+  rails new todo
   cd /apps/todo
-  cp /tmp/database.yml config/database.yml
-  script/generate scaffold Task name:string done_at:datetime
+  rails generate scaffold Task name:string done_at:datetime
   rake db:migrate
   echo 'created new project'
 fi
+
 cd /apps/todo
-exec "$@"
+
+$@
