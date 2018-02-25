c6d01c79c6a02a4a35103b6c10299baae754ead2
ta1kt0me <p.wadachi@gmail.com>
2018-02-26 01:29:11 +0900

    Ruby 1.9

--- app/setup.sh
+++ app/setup.sh
@@ -6,6 +6,7 @@ if [ ! -d "/apps/todo" ]; then
   cd /apps
   rails new todo
   cd /apps/todo
+  sed -i -e "s/^# gem 'ruby-debug19'.*/gem 'pry'/" Gemfile
   rails generate scaffold Task name:string done_at:datetime
   rake db:migrate
   echo 'created new project'
